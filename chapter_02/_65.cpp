d3d90a716cf4586a01a051a213542485028735aa
CoAAColA <50353180+CoAAColA@users.noreply.github.com>
2020-07-02 10:51:01 +0800

    Add files via upload

--- chapter_02/_65.cpp
+++ chapter_02/_65.cpp
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 int odd_ones(unsigned x){
-	return (x & 0xAAAAAAAA) && 1; //assume that index start with 0
-}
-
-int odd_ones_1(unsigned x){
-	return (x & 0x55555555) && 1; //assume that index start with 1
+	x ^= (x >> 16);
+	x ^= (x >> 8);
+	x ^= (x >> 4);
+	x ^= (x >> 2);
+	x ^= (x >> 1);
+	return x&1;
 }
 
 int main(){
-	printf("%d\n", odd_ones(14));
+	printf("%d\n", odd_ones(11));
+	printf("%d\n", odd_ones(0x11111111));
 	return 0;
 }
